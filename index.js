233f43399d1619c1a9df4e7d2abf568020b2fb5b
Shailendrashailendra7571 <114173120+Shailendrashailendra7571@users.noreply.github.com>
2023-08-24 00:17:35 -0700

    add file

---
+++ index.js
@@ -0,0 +1,8 @@
+const root=document.getElementById("root")
+root.addEventListener("click", (e) =>{
+    e.target.style.color="green"
+    e.target.style.fontSize="30px"
+    e.target.style.fontWeight="900"
+    e.target.style.padding="30px"
+    e.target.style.backgroundColor="tomato"
+})
